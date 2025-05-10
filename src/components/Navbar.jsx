75371b0ddba9f7957702b94199fc511eaed776e0
Awais-Baloch <awaisbaloch7753@gmail.com>
2025-05-10 11:48:56 +0500

    updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ function Navbar() {
         <nav className="bg-blue-600 text-white py-4 shadow-md sticky top-0 z-50">
             <div className="flex items-center justify-between px-4">
 
-                <Link className="text-3xl font-bold">Job Portal</Link>
+                <Link to="/" className="text-3xl font-bold">Job Portal</Link>
 
                 <ul className="flex space-x-6">
                     <li><Link to="/" className="hover:text-gray-700 font-medium ">Home</Link></li>
@@ -17,7 +17,7 @@ function Navbar() {
                     <li><Link to="/profile" className="hover:text-gray-700 font-medium">profile</Link></li>
                     <li><Link to="/" className="hover:text-gray-700 font-medium">Logout</Link></li>
                 </ul>
-                
+                 
             </div>
         </nav>
     )
